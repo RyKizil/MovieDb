67499cb30c8dd04797f4795c580a510787a59296
RyKizil <eray_kizilmese@hotmail.com>
2019-03-05 20:41:54 +0100

    Add project files.

---
+++ RazorPagesMovie/Models/SeedData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RazorPagesMovie.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesMovieContext(
+                serviceProvider.GetRequiredService<DbContextOptions<RazorPagesMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When I met Sally",
+                        ReleaseDate = DateTime.Parse("1972-02-15"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M,
+                        Rating = "All"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M,
+                        Rating = "PR"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M,
+                        Rating = "R"
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
